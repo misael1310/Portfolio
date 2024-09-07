ceee81d0840d5deefacc5dde359fa11ec567de4c
Misael <misaelperezoficial@gmail.com>
2024-09-06 20:10:51 -0400

    feat(design): Refactored the Icon component.

--- src/components/CustomIcon/Icon.tsx
+++ src/components/CustomIcon/Icon.tsx
@@ -1,18 +1,12 @@
-interface iconProps {
+export interface iconProps {
   src: string;
   alt: string;
   width: number;
   height: number;
 }
 
-export default function Icon(props: iconProps) {
+export default function Icon({ src, alt, width = 24, height = 24 }: iconProps) {
   return (
-    <img
-      src={props.src}
-      loading="lazy"
-      alt={props.alt}
-      width={props.width}
-      height={props.height}
-    ></img>
+    <img src={src} loading="lazy" alt={alt} width={width} height={height} />
   );
 }
