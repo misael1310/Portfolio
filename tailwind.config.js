061281f5fe960e66ebc2c80c76bc6a111c284e11
Misael <misaelperezoficial@gmail.com>
2024-09-13 23:58:03 -0400

    feat(design): added two custom grid-template-columns for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        stack: "repeat(auto-fit,minmax(160px,160px))",
+        "stack-4": "repeat(4,minmax(160px,160px))",
+      },
       colors: {
         timberwolf: "#d8d7cc",
         ebony: "#212939",
