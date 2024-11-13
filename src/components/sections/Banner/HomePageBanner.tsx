6eeeec14b3325eccc11082eebe2314435c88c045
Misael Perez <misaelperezoficial@gmail.com>
2024-11-13 13:01:21 -0500

    style: set HomePageBanner to 100% screen height

--- src/components/sections/Banner/HomePageBanner.tsx
+++ src/components/sections/Banner/HomePageBanner.tsx
@@ -6,11 +6,11 @@ import { Description } from "./Description";
 
 export default function HomePageBanner() {
   return (
-    <div className="flex flex-wrap justify-center">
-      <div className="w-full lg:w-1/3 flex justify-center">
+    <div className="flex flex-wrap justify-center lg:items-center h-screen">
+      <div className="w-full lg:w-1/3 flex justify-center items-center">
         <ProfilePicture />
       </div>
-      <div className="w-full lg:w-2/3 mt-6">
+      <div className="w-full lg:w-2/3">
         <PersonalDetails />
         <Description />
         <div className="flex items-center gap-5 py-4 px-6 lg:px-0">
