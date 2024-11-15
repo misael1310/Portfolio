05641043a06602592e5ff7b8a6682f2c0e3b268c
Misael <misaelperezoficial@gmail.com>
2024-11-15 15:26:03 -0500

    feat: add About Me section

---
+++ src/data/aboutMeData.ts
@@ -0,0 +1,60 @@
+export const aboutMeContent = [
+  {
+    id: 1,
+    fragments: [
+      { text: 'I’m powered by ' },
+      { text: '5 passions', bold: true },
+      { text: ': computers, soccer, music, science, and coffee.' },
+    ],
+  },
+  {
+    id: 2,
+    fragments: [
+      { text: 'My dream? ' },
+      { text: 'A geek cave', bold: true },
+      { text: ' filled with ThinkPads and ThinkCentres to tinker with!' },
+    ],
+  },
+  {
+    id: 3,
+    fragments: [
+      {
+        text: 'I’ve cheered for Real Madrid since 2011, basking in the glory of 6 UCL wins—',
+      },
+      { text: 'Hala Madrid!', bold: true },
+    ],
+  },
+  {
+    id: 4,
+    fragments: [
+      { text: 'My favorite music genre is ' },
+      { text: 'classical music', bold: true },
+      {
+        text: '. I prefer Mozart over Beethoven, and I love Il Divo and Queen. I think ',
+      },
+      { text: 'Freddie Mercury', bold: true },
+      { text: ' is the best singer who ever existed.' },
+    ],
+  },
+  {
+    id: 5,
+    fragments: [
+      { text: 'I’m obsessed with the ' },
+      { text: 'scientific method', bold: true },
+      {
+        text: ', love geeking out over measuring gadgets, and can’t wait to roam with a Geiger counter, ',
+      },
+      { text: 'hunting radiation', bold: true },
+      { text: ' like it’s Pokémon Go.' },
+    ],
+  },
+  {
+    id: 6,
+    fragments: [
+      {
+        text: 'Coffee fuels my madness—my favorite method is the V60, and I am leveling up with the Syphon. ',
+      },
+      { text: 'Life tastes better brewed!', bold: true },
+    ],
+  },
+];
