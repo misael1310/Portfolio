90502e11b5acec231b476f78030c01ef53107828
Misael Perez <misaelperezoficial@gmail.com>
2024-11-18 13:38:45 -0500

    style: wrap Projects in a section tag and justify the Projects title

--- src/components/sections/Projects/Projects.tsx
+++ src/components/sections/Projects/Projects.tsx
@@ -18,24 +18,28 @@ const projectCard: ProjectCardProps = {
 export default function Projects() {
   const { title, description, picture } = projectCard;
   return (
-    <div className="flex flex-col flex-wrap justify-center -mt-20 pb-10">
-      <div className="flex flex-wrap justify-center my-10">
-        <h2 className="text-4xl lg:text-5xl text-ebony font-bold">Projects</h2>
-      </div>
-      <div className="flex flex-wrap justify-center items-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 px-4">
-          {Array(4)
-            .fill(0)
-            .map((_, index) => (
-              <ProjectCard
-                key={index}
-                title={title}
-                description={description}
-                picture={picture}
-              />
-            ))}
+    <section id="projects" className="max-w-screen-xl m-auto">
+      <div className="flex flex-col flex-wrap justify-center -mt-20 pb-10">
+        <div className="my-10 px-6">
+          <h2 className="text-4xl text-center lg:text-start lg:text-5xl text-ebony font-bold">
+            Projects
+          </h2>
+        </div>
+        <div className="flex flex-wrap justify-center items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 px-4">
+            {Array(4)
+              .fill(0)
+              .map((_, index) => (
+                <ProjectCard
+                  key={index}
+                  title={title}
+                  description={description}
+                  picture={picture}
+                />
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
