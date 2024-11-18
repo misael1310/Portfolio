c81655129fda2daf21588a7b0c955c809c41dd2d
Misael Perez <misaelperezoficial@gmail.com>
2024-11-18 15:02:45 -0500

    style: wrap Profile banner in a section tag and adjust its position

--- src/components/sections/Banner/HomePageBanner.tsx
+++ src/components/sections/Banner/HomePageBanner.tsx
@@ -6,17 +6,19 @@ import { Description } from "./Description";
 
 export default function HomePageBanner() {
   return (
-    <div className="flex flex-wrap justify-center lg:items-center h-screen">
-      <div className="w-full lg:w-1/3 flex justify-center items-center">
-        <ProfilePicture />
-      </div>
-      <div className="w-full lg:w-2/3">
-        <PersonalDetails />
-        <Description />
-        <div className="flex items-center gap-5 py-4 px-6 lg:px-0">
-          <SocialMedia links={socialLinks} />
+    <section id="profile" className="md:-mt-20">
+      <div className="flex flex-wrap justify-center lg:items-center h-screen">
+        <div className="w-full lg:w-1/3 flex justify-center items-center">
+          <ProfilePicture />
+        </div>
+        <div className="w-full lg:w-2/3">
+          <PersonalDetails />
+          <Description />
+          <div className="flex items-center gap-5 py-4 px-6 lg:px-0">
+            <SocialMedia links={socialLinks} />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
