1a6f6b3794ebd1fe4ced3044a158c356ca129e14
Misael Perez <misaelperezoficial@gmail.com>
2024-11-18 16:17:41 -0500

    feat: add Footer Section

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <section style={{ backgroundColor: "#141923" }} id="footer">
+      <footer className="flex justify-center items-center py-4">
+        <p className="text-white">Made with love ❤️ by Mp</p>
+      </footer>
+    </section>
+  );
+}
