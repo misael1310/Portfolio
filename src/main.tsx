37fc2eec86c596a48e99b80824e948cddaec907d
Misael Perez <misaelperezoficial@gmail.com>
2024-12-04 23:54:05 -0500

    chore: replace App component with Router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import Router from "./router.tsx";
 import "./styles/index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.tsx";
@@ -11,7 +11,7 @@ if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <Provider store={store}>
-        <App />
+        <Router />
       </Provider>
     </StrictMode>,
   );
