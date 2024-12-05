02d39d739d623c8d9ab00fb167ca6ea2c3ee4c00
Misael Perez <misaelperezoficial@gmail.com>
2024-12-04 19:14:51 -0500

    feat: complete structure and design of HomePage component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,15 @@
 import HomePageBanner from "../../components/sections/Banner";
 import StackSection from "../../components/sections/Stack";
+import Projects from "../../components/sections/Projects";
+import AboutMe from "../../components/sections/AboutMe";
 
 export default function HomePage() {
   return (
     <div>
       <div className="container mx-auto mt-5">
         <HomePageBanner />
+        <Projects />
+        <AboutMe />
       </div>
       <StackSection />
     </div>
