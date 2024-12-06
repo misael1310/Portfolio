8c546d3c81246c320fba3ab0da367f0e8f973f07
Misael Perez <misaelperezoficial@gmail.com>
2024-12-06 00:25:06 -0500

    feat: implement ScrollToHash Component on Home Page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,16 +2,18 @@ import HomePageBanner from "../../components/sections/Banner";
 import StackSection from "../../components/sections/Stack";
 import Projects from "../../components/sections/Projects";
 import AboutMe from "../../components/sections/AboutMe";
+import ScrollToHash from "../../components/common/ScrollToHash";
 
 export default function HomePage() {
   return (
-    <div>
+    <>
+      <ScrollToHash />
       <div className="container mx-auto mt-5">
         <HomePageBanner />
         <Projects />
         <AboutMe />
       </div>
       <StackSection />
-    </div>
+    </>
   );
 }
