4bd2ef31c012e2a374f9da864345c252a4cc5593
Misael Perez <misaelperezoficial@gmail.com>
2024-12-10 13:34:57 -0500

    chore: add Post Skeleton Loading to Post Component

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -2,6 +2,7 @@ import PostBanner from "./Components/PostBanner";
 import PostContent from "./Components/PostContent";
 import PostTitle from "./Components/PostTitle";
 import NotFoundPage from "../NotFoundPage/NotFoundPage";
+import PostLoading from "./Components/PostLoading";
 import { useParams } from "react-router";
 import { useFetchPostByIDQuery } from "../../features/posts/postsApi-slice";
 
@@ -10,26 +11,34 @@ export default function PostPage() {
   const { data, error, isLoading } = useFetchPostByIDQuery(id as string);
 
   const renderContent = () => {
-    if (isLoading) return "Loading...";
+    if (isLoading) return <PostLoading />;
 
     if (error) {
       if ("status" in error && error.status === 404) {
-        return <NotFoundPage />;
+        return (
+          <div className="mt-20">
+            <NotFoundPage />
+          </div>
+        );
       }
       const errorMessage = "Something went wrong...";
       if ("data" in error && (error.data as { message?: string }).message) {
         return `${errorMessage} ${(error.data as { message: string }).message}`;
       }
-      return errorMessage;
+      return (
+        <div className="mt-20">
+          <h1>{errorMessage}</h1>
+        </div>
+      );
     }
 
     if (data) {
       return (
-        <>
+        <div className="pt-20">
           <PostBanner {...data} />
           <PostTitle title={data.post_title} />
           <PostContent />
-        </>
+        </div>
       );
     }
     return null;
