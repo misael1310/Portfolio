bf6689417ae8817ea9f3f2349e103eb504840bc5
Misael Perez <misaelperezoficial@gmail.com>
2024-12-18 19:18:02 -0500

    feat: add Static Project Loading Card Data to Assets

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -8,4 +8,43 @@ const Icons = {
   LOCATION: 'https://www.svgrepo.com/show/532540/location-pin-alt-1.svg',
 };
 
-export { Images, Icons };
+const PROJECTS_CARDS_LOADING = [
+  {
+    passion: '💻',
+    logo: {
+      src: 'https://i.ibb.co/kcpgbPY/MP-icon.webp',
+      srcPng: 'https://i.ibb.co/zNQ8Nq3/MP-icon.png',
+      title: 'Passion',
+      alt: 'Misael Perez (MP Dev) Logo',
+    },
+  },
+  {
+    passion: '⚽',
+    logo: {
+      src: 'https://i.ibb.co/Qjdwx4S/Real-Madrid-CF-icon.webp',
+      srcPng: 'https://i.ibb.co/DKXRtJz/Real-Madrid-CF-icon.png',
+      title: 'Real Madrid',
+      alt: 'Real Madrid Club de Fútbol Logo',
+    },
+  },
+  {
+    passion: '🎹',
+    logo: {
+      src: 'https://i.ibb.co/FwJyd7V/Mozart-logo-icon.webp',
+      srcPng: 'https://i.ibb.co/59wtJBY/Mozart-logo-icon.png',
+      title: 'Mozart',
+      alt: 'Wolfgang Amadeus Mozart figure – classical composer illustration',
+    },
+  },
+  {
+    passion: '🧮',
+    logo: {
+      src: 'https://i.ibb.co/tbf6CtS/gravity-icon.webp',
+      srcPng: 'https://i.ibb.co/t8sVWWv/gravity-icon.png',
+      title: 'Newton',
+      alt: 'An apple figure representing Newton’s discovery of gravity.',
+    },
+  },
+];
+
+export { Images, Icons, PROJECTS_CARDS_LOADING };
